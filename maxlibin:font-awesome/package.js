5ba4e23ad710f6407e45fa68e929065f90875bb1
max <max.li.bin@ahrefs.com>
2015-01-08 18:09:05 +0800

    add client to addFile

--- maxlibin:font-awesome/package.js
+++ maxlibin:font-awesome/package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'maxlibin:font-awesome',
   summary: 'font-awesome CDN',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/meteor-singapore/font-awesome'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
   api.use('templating', 'client');
-  api.addFiles('font-awesome.js');
+  api.addFiles('font-awesome.js', 'client');
 });
